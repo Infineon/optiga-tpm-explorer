8c408b60040b8121b86693dc55cff82c93cfe76a
RaymWong <46432420+RaymWong@users.noreply.github.com>
2022-03-07 21:17:43 +0800

    1)Updated binary pacakge generation shell script

--- Python_TPM20_GUI/create_binary_package.sh
+++ Python_TPM20_GUI/create_binary_package.sh
@@ -1,15 +1,17 @@
-python -m py_compile *.py
+python3 -m compileall -b .
 sudo rm -r bin 
 mkdir bin
 mkdir ./bin/working_space
+mkdir ./bin/images
 cp *.pyc ./bin/
 cp eHealthDevice ./bin/
 cp ./working_space/*.jsn  ./bin/working_space
 cp ./working_space/*.sh  ./bin/working_space
 cp ./working_space/AmazonRootCA1.* ./bin/working_space
+cp ./images/*.png ./bin/images
 
 echo "sudo chmod 777 eHealthDevice" >> ./bin/start_gui.sh
-echo "python main.pyc" >> ./bin/start_gui.sh
+echo "python3 main.pyc" >> ./bin/start_gui.sh
 
 git log -1 --format="%H" >> ./bin/commit_info
 sudo chmod 777 ./bin/start_gui.sh
