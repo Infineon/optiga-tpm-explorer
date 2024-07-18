514a4c8db95368edc1e711ea48b14c58dfe73b7b
RaymWong <46432420+RaymWong@users.noreply.github.com>
2024-07-18 17:30:52 +0800

    Add symlink shorcut to start gui script

--- Python_TPM20_GUI/create_binary_package.sh
+++ Python_TPM20_GUI/create_binary_package.sh
@@ -10,9 +10,14 @@ cp ./working_space/*.sh  ./bin/working_space
 cp ./working_space/AmazonRootCA1.* ./bin/working_space
 cp ./images/*.png ./bin/images
 
+BASEDIR=$(realpath $(dirname $0))
+
+echo "cd ${BASEDIR}/bin" >> ./bin/start_gui.sh
 echo "sudo chmod 777 eHealthDevice" >> ./bin/start_gui.sh
 echo "python3 main.pyc" >> ./bin/start_gui.sh
 
 git log -1 --format="%H" >> ./bin/commit_info
 sudo chmod 777 ./bin/start_gui.sh
 rm *.pyc
+sudo rm ${BASEDIR}/start_gui.sh 2> /dev/null
+sudo ln -s ${BASEDIR}/bin/start_gui.sh  ${BASEDIR}/start_gui.sh
